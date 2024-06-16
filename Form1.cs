e6e7b56320a115bda33faca26a4655282674aa0d
jessa <jessa@PAMMMYYY>
2024-06-16 16:20:22 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace EmployeeNamespace
+{
+    public partial class frmComputeSalary : Form
+    {
+        public frmComputeSalary()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fName = textBox1.Text;
+            string lName = textBox2.Text;
+            string dept = textBox3.Text;
+            string jTitle = textBox4.Text;
+           
+
+            PartTimeEmployee PTE = new PartTimeEmployee(fName, lName, dept, jTitle);
+
+            PTE.computeSalary(Convert.ToDouble(textBox5.Text), Convert.ToInt32(textBox6.Text));
+
+            label10.Text = fName.ToString();
+            label11.Text = lName.ToString();
+            label12.Text = PTE.BasicSalary.ToString();         
+
+        }
+    }
+}
---
+++ PartTimeEmployee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeInterface;
+
+namespace EmployeeNamespace
+{
+    public class PartTimeEmployee : IEmployee
+    {
+        private string first_name;
+        private string last_name;
+        private string department;
+        private string job_title;
+        private double basic_salary;
+
+        public string FirstName
+        {
+            get { return this.first_name; }
+            set { this.first_name = value; }
+        }
+        public string LastName { 
+            get { return this.last_name; }
+            set { this.last_name = value;}
+        }
+        public string Department {
+            get { return this.department; }
+            set { this.department = value; }
+        }
+        public string JobTitle { 
+            get { return this.job_title; }
+            set { this.job_title = value; }
+        }
+        public double BasicSalary {
+            get { return this.basic_salary; }
+            set { this.basic_salary = value;}
+        }
+
+        public PartTimeEmployee(string Fname, string Lname, string dept, string job) {
+            this.first_name = Fname;
+            this.last_name = Lname;
+            this.department = dept;
+            this.job_title = job;
+        }
+        public void computeSalary(double ratePerHour, int hoursWorked) { 
+            this.basic_salary = ratePerHour * hoursWorked;
+            
+        }
+        public double getSalary() {
+            return this.basic_salary;
+        }
+
+
+    }
+}
